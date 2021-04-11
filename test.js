3bb2ccab15157047dce3b7800964008633448540
MrAugu <mraugu@yahoo.com>
2021-04-11 10:46:29 +0300

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const Clock = require("./dist/lib/Clock").default;
-const myClock = new Clock({ interval: 1000 });
-console.time("is");
-myClock.schedule(Date.now() + 5000, { hello: "world" });
-console.timeEnd("is");
-
-myClock.on("done", (data) => console.log(`Hello ${data.hello}.`));
